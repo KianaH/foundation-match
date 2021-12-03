dd8a7239a9cc8176b7f9119edb1834e31967b632
Kiana Harrell <kjh234@cornell.edu>
2021-12-02 23:07:48 -0800

    Home page for foundation search; added search bar and submission form

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,8 +1,24 @@
+import FoundationList from './FoundationList';
+
+function SearchBar() {
+  return (
+    <>
+      <form>
+        <input class="border-purple-400" type="text" placeholder="Search..." />
+        <input type="submit" value="Search" />
+      </form>
+    </>
+  )
+}
 
 function App() {
+  const [foundations, setFoundations] = useState([])
+
   return (
     <div>
       <h1>Foundation Match</h1>
+      <SearchBar />
+      {foundations && <FoundationList list={foundations}/>}
     </div>
   );
 }
---
+++ ui/src/FoundationList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function FoundationList({ list }) {
+  
+}
+
+export default FoundationList;
